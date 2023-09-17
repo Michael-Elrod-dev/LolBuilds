e341aa074b8c8a853ab0889cb851bdf1dcdb4741
Michael-Elrod-dev <michaelelrod.dev@gmail.com>
2023-09-17 09:15:14 -0400

    Switched to next.js

---
+++ src/components/header.js
@@ -0,0 +1,23 @@
+function Header() {
+    return (
+      <header className="bg-gray-800 p-4">
+        <div className="container mx-auto">
+          <nav className="flex justify-between items-center">
+            <div className="text-white font-bold text-xl">U.GG Clone</div>
+            <ul className="flex space-x-4">
+              <li><a href="#" className="text-white">Tier List</a></li>
+              <li><a href="#" className="text-white">Champions</a></li>
+              <li><a href="#" className="text-white">Multisearch</a></li>
+              <li><a href="#" className="text-white">Live Games</a></li>
+              <li><a href="#" className="text-white">Leaderboards</a></li>
+              <li><a href="#" className="text-white">Items</a></li>
+              <li><a href="#" className="text-white">Probuild Stats</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    );
+  }
+  
+  export default Header;
+  
