1c2e5749e17cb1b71cbdfd5eba36d87ae4c1e860
Michael-Elrod-dev <michaelelrod.dev@gmail.com>
2023-09-17 11:35:02 -0400

    update

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,20 @@
 function Header() {
-    return (
-      <header className="bg-gray-800 p-4">
-        <div className="container mx-auto">
-          <nav className="flex justify-between items-center">
-            <div className="text-white font-bold text-xl">U.GG Clone</div>
-            <ul className="flex space-x-4">
-              <li><a href="#" className="text-white">Tier List</a></li>
-              <li><a href="#" className="text-white">Champions</a></li>
-              <li><a href="#" className="text-white">Multisearch</a></li>
-              <li><a href="#" className="text-white">Live Games</a></li>
-              <li><a href="#" className="text-white">Leaderboards</a></li>
-              <li><a href="#" className="text-white">Items</a></li>
-              <li><a href="#" className="text-white">Probuild Stats</a></li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-    );
-  }
-  
+  return (
+    <header className="bg-black w-full p-4 flex justify-center">
+      <nav className="flex justify-between items-center w-full">
+        <div className="text-white font-bold text-xl">U.GG Clone</div>
+        <ul className="flex space-x-4">
+          <li><a href="#" className="text-white">Tier List</a></li>
+          <li><a href="#" className="text-white">Champions</a></li>
+          <li><a href="#" className="text-white">Multisearch</a></li>
+          <li><a href="#" className="text-white">Live Games</a></li>
+          <li><a href="#" className="text-white">Leaderboards</a></li>
+          <li><a href="#" className="text-white">Items</a></li>
+          <li><a href="#" className="text-white">Probuild Stats</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
 export default Header;
